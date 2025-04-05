bd607a704f8d42c2e64e56a574464fdb4d2c5e3e
Tarun Makode <tarunmakode18@gmail.com>
2025-04-05 12:27:26 +0530

    Update App.jsx

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "https://doctor-appointment-system-4-9u4g.onrender.com",
+          "https://doctor-appointment-system-6.onrender.com/api/v1/user/admin/me",
           {
             withCredentials: true,
           }
