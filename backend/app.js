ff8268a764856e423579fe5afe48def064b890e2
Tarun Makode <tarunmakode18@gmail.com>
2025-04-05 11:12:56 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [FRONTEND_URL_ONE,FRONTEND_URL_TWO],
+    origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
