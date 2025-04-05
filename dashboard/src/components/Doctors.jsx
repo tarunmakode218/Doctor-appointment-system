7c3237e315345f831682ccacc610b432eca988a2
Tarun Makode <tarunmakode18@gmail.com>
2025-04-05 09:46:14 +0530

    Update Doctors.jsx

--- dashboard/src/components/Doctors.jsx
+++ dashboard/src/components/Doctors.jsx
@@ -85,7 +85,7 @@ const Doctors = () => {
     const fetchDoctors = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/user/doctors",
+          "https://doctor-appointment-system-7zeg.onrender.com",
           { withCredentials: true }
         );
         setDoctors(data.doctors);
