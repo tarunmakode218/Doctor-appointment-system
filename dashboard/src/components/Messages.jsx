c0677fe45a9e24f772af918a713848dc8b342732
Tarun Makode <tarunmakode18@gmail.com>
2025-04-05 12:24:48 +0530

    Update Messages.jsx

--- dashboard/src/components/Messages.jsx
+++ dashboard/src/components/Messages.jsx
@@ -11,7 +11,7 @@ const Messages = () => {
     const fetchMessages = async () => {
       try {
         const { data } = await axios.get(
-          "https://doctor-appointment-system-4-9u4g.onrender.com",
+          "https://doctor-appointment-system-6.onrender.com/api/v1/message/getall",
           { withCredentials: true }
         );
         setMessages(data.messages);
