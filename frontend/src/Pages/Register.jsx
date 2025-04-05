341a31f3fe2e20f4d5423fa17df6f63587521c22
Tarun Makode <tarunmakode18@gmail.com>
2025-04-05 09:34:24 +0530

    Update Register.jsx

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/patient/register",
+          "https://doctor-appointment-system-7zeg.onrender.com",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
