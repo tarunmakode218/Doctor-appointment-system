f4beb6e95337b403d218ad7f1cab4bd8540b47ca
Tarun Makode <tarunmakode18@gmail.com>
2025-04-05 11:21:57 +0530

    Update AddNewDoctor.jsx

--- dashboard/src/components/AddNewDoctor.jsx
+++ dashboard/src/components/AddNewDoctor.jsx
@@ -58,7 +58,7 @@ const AddNewDoctor = () => {
       formData.append("doctorDepartment", doctorDepartment);
       formData.append("docAvatar", docAvatar);
       await axios
-        .post("https://doctor-appointment-system-7zeg.onrender.com", formData, {
+        .post("https://doctor-appointment-system-4-9u4g.onrender.com", formData, {
           withCredentials: true,
           headers: { "Content-Type": "multipart/form-data" },
         })
