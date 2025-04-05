3b938a7c3c90ef160230e5d5e646f1062729dc0f
Tarun Makode <tarunmakode18@gmail.com>
2025-04-05 09:35:02 +0530

    Update AppointmentForm.jsx

--- frontend/src/components/AppointmentForm.jsx
+++ frontend/src/components/AppointmentForm.jsx
@@ -47,7 +47,7 @@ const AppointmentForm = () => {
     try {
       const hasVisitedBool = Boolean(hasVisited);
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/appointment/post",
+        "https://doctor-appointment-system-7zeg.onrender.com",
         {
           firstName,
           lastName,
