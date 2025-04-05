fadc227ea4ac3bd5dee3ce42e1bee7a3ef859963
Tarun Makode <tarunmakode18@gmail.com>
2025-04-05 12:16:57 +0530

    Update AddNewAdmin.jsx

--- dashboard/src/components/AddNewAdmin.jsx
+++ dashboard/src/components/AddNewAdmin.jsx
@@ -23,7 +23,7 @@ const AddNewAdmin = () => {
     try {
       await axios
         .post(
-          "https://doctor-appointment-system-6.onrender.com",
+          "https://doctor-appointment-system-6.onrender.com/api/v1/user/admin/addnew",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
